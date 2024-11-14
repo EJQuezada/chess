03afe28a1119bf2e11dffd52eeebce876ba23397
Edgar Quezada <edgar.quezada@gmail.com>
2024-11-14 14:30:12 +0100

    update Ranks.js file

--- src/components/Board/bits/Ranks.js
+++ src/components/Board/bits/Ranks.js
@@ -1,6 +1,6 @@
 const Ranks = ({ranks}) => (
     return <div className="ranks">
-        {ranks.map(rank => <span>{rank}</span>)}
+        {ranks.map(rank => <span key={rank}>{rank}</span>)}
     </div>
 )
 
