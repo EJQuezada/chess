070fe1c7d4e619985b993640d7fe28bec04035ac
Edgar Quezada <edgar.quezada@gmail.com>
2024-11-14 13:52:42 +0100

    update Board.js file

--- src/components/Board/Board.js
+++ src/components/Board/Board.js
@@ -3,6 +3,12 @@ import './Board.css'
 
 const Board = () => {
 
+    const getClassName = (i,j) => {
+        let c = 'tile'
+        c+= (i+j) % 2 === 0 ? ' tile--light' : ' tile--dark'
+        return c
+    }
+
     const ranks = Array(8).fill().map((x,i) => 8-i)
     const files = Array(8).fill().map((x,i) => getCharacter(i))
 
@@ -11,7 +17,7 @@ const Board = () => {
         <div className='tiles'>
             {ranks.map((rank,i) =>
                 files.map((file,j) => 
-                    <div key={file+'-'+rank}>{rank}{file}</div>
+                    <div key={file+'-'+rank} className={getClassName(i,j)}>{rank}{file}</div>
                 )
             )} 
         </div>
