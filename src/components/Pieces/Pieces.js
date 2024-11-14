2c656169d939a9a5724e6659eae486624c1d61ef
Edgar Quezada <edgar.quezada@gmail.com>
2024-11-14 17:37:00 +0100

    update Pieces.js file

--- src/components/Pieces/Pieces.js
+++ src/components/Pieces/Pieces.js
@@ -1,4 +1,5 @@
 import './Pieces.css'
+import Piece from './Piece'
 
 const Pieces = () => {
     const position = new Array(8).fill('').map(x=> new Array(8).fill(''))
@@ -14,7 +15,12 @@ const Pieces = () => {
         {position.map((r,rank) => 
             r.map((f,file) =>
                 position[rank][file]
-                ?   position[rank][file]
+                ?   <Piece
+                        key={rank+'-'+file}                        }
+                        rank={rank}
+                        file={file}
+                        piece={position[rank][file]}
+                    >
                 :   null
         ))}
 
