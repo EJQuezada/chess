706c84267ca8cdaabbe2cea1d6f05c5e66827358
Edgar Quezada <edgar.quezada@gmail.com>
2024-11-15 15:19:01 +0100

    add setTimeout function to Piece.js file

--- src/components/Pieces/Piece.js
+++ src/components/Pieces/Piece.js
@@ -9,6 +9,9 @@ const Piece = ({
     const onDragStart = e => {
         e.dataTransfer.effectAllowed = 'move'
         e.dataTransfer.setData('text/plain',`${piece},${rank},${file}`)
+        setTimeout(() => {
+            e.target.style.display = 'none'
+        },0)
     }
 
     return (
