01c74a4f4be4e204c1041c3abb5f881fc61b4ab2
Edgar Quezada <edgar.quezada@gmail.com>
2024-11-27 13:12:55 +0100

    update piece.js file with getMoves function

--- src/components/Pieces/Piece.js
+++ src/components/Pieces/Piece.js
@@ -1,15 +1,45 @@
-
+import { useAppContext } from "../../contexts/Context"
 const Piece = ({
     rank, 
     file, 
     piece,
 }) => {
+
+    const {appState, dispatch} = useAppContext()
+    const {turn, position} = appState;
+    const currentPosition = position[position.length - 1]
+
+    const getMoves = () => {
+        const moves = []
+        const us = piece[0]
+        const enemy = us === 'w' ? 'b' : 'w'
+
+        const direction = [
+            [-1,0],
+            [1,0],
+            [0,-1],
+            [0,1],
+        ]
+
+        direction.forEach(dir => {
+            for (let i = 0; i < 8; i++) {
+                const x = rank + (i*dir[0])
+                const y = file + (i*dir[1])
+                if (currentPosition[x][y] === undefined)
+                
+            }
+        })
+    }
+
     const onDragStart = e => {
         e.dataTransfer.effectAllowed = 'move'
         e.dataTransfer.setData('text/plain',`${piece},${rank},${file}`)
         setTimeout(() => {
             e.target.style.display = 'none'
         },0)
+        if (turn === piece[0]) {
+            const candidateMoves = getMoves()
+        }
     }
     const onDragEnd = e => {
         e.target.style.display = 'block'
@@ -19,8 +49,8 @@ const Piece = ({
         <div 
             className={`piece ${piece} p-${file}${rank}`}
             draggable={true}
-            onDragEnd={onDragEnd}
             onDragStart={onDragStart}
+            onDragEnd={onDragEnd} 
         
         />)
 }
